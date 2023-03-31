0cabcb421942ab632cce30c153e73a0f2ad8323a
Massick <4ndyraul@gmail.com>
2023-03-30 23:29:03 -0400

    add commands middleware

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { logger } from "./logger/index.js";
 import { TOKEN } from "./config/index.js";
 import { validator } from "./middleware/validator.js";
 import { start } from "./middleware/start.js";
+import { commands } from "./middleware/commands.js";
 
 const bot = new Telegraf(TOKEN);
 
@@ -10,7 +11,7 @@ bot.command('test', ctx => {
     ctx.reply('tested', { reply_to_message_id: ctx.message?.message_id })
 })
 
-bot.use(validator).use(start)
+bot.use(validator).use(start).use(commands)
 
 bot.launch()
 logger.success('BOT INICIADO')
