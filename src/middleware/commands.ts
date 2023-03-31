4807161cc40482dde1efeae519bef9fcae31afd7
Massick <4ndyraul@gmail.com>
2023-03-30 23:08:23 -0400

    add basic followers command

--- src/middleware/commands.ts
+++ src/middleware/commands.ts
@@ -1,7 +1,7 @@
 import { Composer } from "telegraf";
 import { prisma } from "../db/prisma.js";
 import { logger } from "../logger/index.js";
-import { getUserByUsername } from "../utils/twitter.js";
+import { getUserByUsername, getFollowers, getFollowing } from "../utils/twitter.js";
 
 export const commands = new Composer()
 
@@ -44,6 +44,66 @@ commands.command('myid', async ctx => {
         } else {
             ctx.reply('Type your Twitter ID as in <code>/myid 1234567890</code>', {
                 parse_mode: 'HTML'
+            })
+        }
+    }
+})
+
+commands.command('followers', async ctx => {
+    if (ctx.chat.type === 'private') {
+        const tgId = ctx.from.id.toString()
+        const user = await prisma.user.findUnique({
+            where: {
+                tgId,
+            },
+            include: {
+                twitterCircle: true
+            }
+        })
+        if (user && user.twId) {
+            const previousFollowers = user.twitterCircle
+            const previousFollowersIds = previousFollowers.map(f => f.twId)
+            const isFirstTime = previousFollowers.length === 0
+            const followers = await getFollowers(user.twId)
+            let newFollowers = []
+            let noLongerFollowers = previousFollowers
+            if (followers) {
+                for await (const follower of followers) {
+                    await prisma.twUser.upsert({
+                        where: {
+                            twId_userId: {
+                                twId: follower.id,
+                                userId: user.id,
+                            }
+                        },
+                        update: {
+                            name: follower.name,
+                            username: follower.username,
+                            follower: true,
+                        },
+                        create: {
+                            twId: follower.id,
+                            name: follower.name,
+                            username: follower.username,
+                            follower: true,
+                            userId: user.id
+                        }
+                    })
+
+                    if (!previousFollowersIds.includes(follower.id)) {
+                        newFollowers.push(follower)
+                    }
+                    else {
+                        noLongerFollowers = noLongerFollowers.filter(f => f.twId !== follower.id)
+                    }
+                }
+            }
+
+            const text = `You have ${newFollowers.length} new followers\n${noLongerFollowers.length} users stop following you\n\nTotal: ${previousFollowers.length + newFollowers.length - noLongerFollowers.length}`
+            ctx.reply(text)
+        } else {
+            ctx.reply('It seems like you haven\'t set you Twitter ID. You can use <code>/get username</code>, where <code>username</code> is your Twitter handle, to get your Twitter ID, then use <code>/myid id</code> replacing <code>id</code> with the value you just got and then try this command again.', {
+                parse_mode: "HTML"
             })
         }
     }
