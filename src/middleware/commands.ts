dbbf3dec697b9491f87292c4a1cd3b1f7f96f381
Massick <4ndyraul@gmail.com>
2023-03-30 23:28:42 -0400

    add following command and fix following

--- src/middleware/commands.ts
+++ src/middleware/commands.ts
@@ -61,7 +61,7 @@ commands.command('followers', async ctx => {
             }
         })
         if (user && user.twId) {
-            const previousFollowers = user.twitterCircle
+            const previousFollowers = user.twitterCircle.filter(u => u.follower)
             const previousFollowersIds = previousFollowers.map(f => f.twId)
             const isFirstTime = previousFollowers.length === 0
             const followers = await getFollowers(user.twId)
@@ -99,11 +99,76 @@ commands.command('followers', async ctx => {
                 }
             }
 
-            const text = `You have ${newFollowers.length} new followers\n${noLongerFollowers.length} users stop following you\n\nTotal: ${previousFollowers.length + newFollowers.length - noLongerFollowers.length}`
+            const text = `You have ${newFollowers.length} new followers\n${noLongerFollowers.length} accounts stop following you\n\nTotal: ${previousFollowers.length + newFollowers.length - noLongerFollowers.length}`
             ctx.reply(text, {
                 parse_mode: 'HTML',
                 ...Markup.inlineKeyboard([
                     Markup.button.callback('Export List', `exportFollowers_${user.id}`)
+                ])
+            })
+        } else {
+            ctx.reply('It seems like you haven\'t set you Twitter ID. You can use <code>/get username</code>, where <code>username</code> is your Twitter handle, to get your Twitter ID, then use <code>/myid id</code> replacing <code>id</code> with the value you just got and then try this command again.', {
+                parse_mode: "HTML"
+            })
+        }
+    }
+})
+
+commands.command('following', async ctx => {
+    if (ctx.chat.type === 'private') {
+        const tgId = ctx.from.id.toString()
+        const user = await prisma.user.findUnique({
+            where: {
+                tgId,
+            },
+            include: {
+                twitterCircle: true
+            }
+        })
+        if (user && user.twId) {
+            const previousFollowings = user.twitterCircle.filter(u => u.following)
+            const previousFollowingsIds = previousFollowings.map(f => f.twId)
+            const isFirstTime = previousFollowings.length === 0
+            const followings = await getFollowing(user.twId)
+            let newFollowings = []
+            let noLongerFollowings = previousFollowings
+            if (followings) {
+                for await (const following of followings) {
+                    await prisma.twUser.upsert({
+                        where: {
+                            twId_userId: {
+                                twId: following.id,
+                                userId: user.id,
+                            }
+                        },
+                        update: {
+                            name: following.name,
+                            username: following.username,
+                            following: true,
+                        },
+                        create: {
+                            twId: following.id,
+                            name: following.name,
+                            username: following.username,
+                            following: true,
+                            userId: user.id
+                        }
+                    })
+
+                    if (!previousFollowingsIds.includes(following.id)) {
+                        newFollowings.push(following)
+                    }
+                    else {
+                        noLongerFollowings = noLongerFollowings.filter(f => f.twId !== following.id)
+                    }
+                }
+            }
+
+            const text = `You are following ${newFollowings.length} new accounts\nYou stopped following ${noLongerFollowings.length} accounts\n\nTotal: ${previousFollowings.length + newFollowings.length - noLongerFollowings.length}`
+            ctx.reply(text, {
+                parse_mode: 'HTML',
+                ...Markup.inlineKeyboard([
+                    Markup.button.callback('Export List', `exportFollowings_${user.id}`)
                 ])
             })
         } else {
