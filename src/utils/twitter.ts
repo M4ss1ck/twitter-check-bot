529ab80198e6aa038c40fd9b6ce94648d665ab87
Massick <4ndyraul@gmail.com>
2023-04-01 18:48:23 -0400

    fix followers/followings getters

--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -1,5 +1,5 @@
 import { User, TwUser } from "@prisma/client";
-import { TwitterApi, UserV2 } from "twitter-api-v2";
+import { TwitterApi, UserV2, ApiResponseError } from "twitter-api-v2";
 import { logger } from "../logger/index.js";
 import { prisma } from "../db/prisma.js";
 
@@ -15,16 +15,65 @@ export const twClient = new TwitterApi({
     accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
 });
 
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function autoRetryOnRateLimitError<T>(callback: () => T | Promise<T>) {
+    while (true) {
+        try {
+            return await callback();
+        } catch (error) {
+            if (error instanceof ApiResponseError && error.rateLimitError && error.rateLimit) {
+                const resetTimeout = error.rateLimit.reset * 1000; // convert to ms time instead of seconds time
+                const timeToWait = resetTimeout - Date.now();
+
+                await sleep(timeToWait);
+                continue;
+            }
+
+            throw error;
+        }
+    }
+}
+
 export const getFollowers = async (id: string) => {
     try {
-        return await twClient.v2.followers(id, { asPaginator: true })
+        // return await twClient.v2.followers(id, { asPaginator: true, max_results: 1000 })
+        let result = await twClient.v2.followers(id, { max_results: 1000 })
+        logger.info(result.meta)
+        logger.error(result.errors)
+        let nextToken = result.meta ? result.meta.next_token : undefined
+        let followers = result.data
+        while (nextToken) {
+            let tempFollowers = await twClient.v2.followers(id, { max_results: 1000, pagination_token: nextToken })
+            tempFollowers.data && followers.push(...tempFollowers.data)
+            nextToken = tempFollowers.meta ? tempFollowers.meta.next_token : undefined
+            logger.info(tempFollowers.meta)
+            logger.error(tempFollowers.errors)
+        }
+        return followers
     } catch (error) {
         logger.error(error)
     }
 }
+
 export const getFollowing = async (id: string) => {
     try {
-        return await twClient.v2.following(id, { asPaginator: true })
+        // return await twClient.v2.following(id, { asPaginator: true })
+        let result = await twClient.v2.following(id, { max_results: 1000 })
+        logger.info(result.meta)
+        logger.error(result.errors)
+        let nextToken = result.meta ? result.meta.next_token : undefined
+        let followings = result.data
+        while (nextToken) {
+            let tempFollowings = await twClient.v2.following(id, { max_results: 1000, pagination_token: nextToken })
+            tempFollowings.data && followings.push(...tempFollowings.data)
+            nextToken = tempFollowings.meta ? tempFollowings.meta.next_token : undefined
+            logger.info(tempFollowings.meta)
+            tempFollowings.errors && logger.error(tempFollowings.errors)
+        }
+        return followings
     } catch (error) {
         logger.error(error)
     }
@@ -42,6 +91,7 @@ export const allFollowerData = async (user: FullUser) => {
     const previousFollowers = user.twitterCircle.filter(u => u.follower)
     const previousFollowersIds = previousFollowers.map(f => f.twId)
     const followers = await getFollowers(user.twId)
+
     let newFollowers = []
     let noLongerFollowers = previousFollowers
     if (followers) {
@@ -99,6 +149,7 @@ export const allFollowingData = async (user: FullUser) => {
     const previousFollowings = user.twitterCircle.filter(u => u.following)
     const previousFollowingsIds = previousFollowings.map(f => f.twId)
     const followings = await getFollowing(user.twId)
+
     let newFollowings = []
     let noLongerFollowings = previousFollowings
     if (followings) {
