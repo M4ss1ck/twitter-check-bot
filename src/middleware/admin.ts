356bb829fd26f2c07d2d54a50bbeb7d7d4a3c47c
Massick <4ndyraul@gmail.com>
2023-04-01 18:48:01 -0400

    fix bug with user id

--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -6,7 +6,7 @@ import { ADMIN_ID } from "../config/index.js";
 export const admin = new Composer()
 
 admin.command('add', Composer.acl(parseInt(ADMIN_ID), async ctx => {
-    const tgId = ctx.message.text.replace(/^\/adduser\s+/i, '').trim()
+    const tgId = ctx.message.text.replace(/^\/add\s+/i, '').trim()
     await prisma.user.upsert({
         where: {
             tgId,
